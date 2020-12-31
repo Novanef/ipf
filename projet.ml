922c0543c1060df30707b62087ff18dd96539795
ouialed <nicolas.venot@ensiie.fr>
2020-12-31 13:45:34 +0100

    connexe

--- projet.ml
+++ projet.ml
@@ -1,29 +1,35 @@
 open Foncteur
 module Tree_R = FoncteurTree(Coord_R)
 open Tree_R
+  (*supprime le membre égal à p de la liste*)
   let rec deletelist p l=match l with 
   |[]->[]
   |r::q->if r=p then q else p::(deletelist p q)
+  (*retourne la base de l'arbre, ie les points avec un bool=true*)
   let getbase t=let rec auxbase t l=match t with
   |Noeud(b,c,tl)->if(b=true) then auxbaseb tl (c::l) else auxbaseb tl l
   and auxbaseb tl l=match tl with
   |[]->l
   |p::q->auxbaseb q (auxbase p l) in auxbase t []
+  (*retourne la longueur de la liste*)
   let lengthlist l=let rec aux l acc=match l with 
   |[]->acc
   |p::q-> aux q (acc+1) in aux l 0
+  (*50% de chance de supprimer un membre aléatoire de la liste*)
   let deleterandom l=if(lengthlist l)>0 then let rec auxrand l x=match l with 
   |[]->[]
   |p::q->if x=0&&(Random.int 2=1) then q else p::(auxrand q (x-1)) 
   in auxrand l (Random.int (lengthlist l)) else []
-  let is_connexe t l=match t with 
-  |Noeud(b,c,tl)->let rec is_accessible c t l=if not(mem l c) then auxaccessible c tl (deletelist c l) else auxaccessible c tl l
-  and auxaccessible p tl l=match tl with
-  |[]-> l=[]
-  |s::u->is_accessible p s l||l=[] 
-  in is_accessible c t []
-
+  (*retourne la liste de coordonnées l moins les points trouvés lors du parcours de t*)
+  let rec is_accessible t l=match t with 
+  |Noeud(b,c,tl)->if not(mem l c) then auxaccessible tl (deletelist c l) else auxaccessible tl l
+  and auxaccessible tl l=match tl with
+  |[]-> l
+  |s::u->auxaccessible u (is_accessible s l)
+  (*retourne true si tous les points de la liste l ont été trouvés*)
+  let is_connexe t l=is_accessible t l=[]
   
+  (*Renvoie un arbre avec certaines arètes ont été aléatoirement supprimées*)
   let rec  generatetree t=match t with 
   |Noeud(b,c,tl)->Noeud(b,c, (auxgen (deleterandom tl) c))
   and auxgen tl c=match tl with
@@ -31,5 +37,6 @@ open Tree_R
   |p::q->match p with 
   |Noeud(a,d,tlb)->if d!=c then (generatetree (Noeud(a,d, tlb)))::(auxgen q d) else p::(auxgen q c)
 
+  (*itération n fois pour trouver un candidat avec un meilleur poids que l'arbre initial*)
   let rec candidate t n=if n>0 then let c=(generatetree t)in if not(findCycle c)||not(is_connexe c (getbase c)) then candidate t n 
   else if (weight t)>(weight c) then candidate c (n-1) else candidate t (n-1) else t
