8444383496829f8a067a09a4f8a8de4c288d75d3
ouialed <nicolas.venot@ensiie.fr>
2020-12-31 12:18:42 +0100

    1ère version fonction candidat

--- projet.ml
+++ projet.ml
@@ -4,6 +4,11 @@ open Tree_R
   let rec deletelist p l=match l with 
   |[]->[]
   |r::q->if r=p then q else p::(deletelist p q)
+  let getbase t=let rec auxbase t l=match t with
+  |Noeud(b,c,tl)->if(b=true) then auxbaseb tl (c::l) else auxbaseb tl l
+  and auxbaseb tl l=match tl with
+  |[]->l
+  |p::q->auxbaseb q (auxbase p l) in auxbase t []
   let lengthlist l=let rec aux l acc=match l with 
   |[]->acc
   |p::q-> aux q (acc+1) in aux l 0
@@ -25,3 +30,6 @@ open Tree_R
   |[]->[]
   |p::q->match p with 
   |Noeud(a,d,tlb)->if d!=c then (generatetree (Noeud(a,d, tlb)))::(auxgen q d) else p::(auxgen q c)
+
+  let rec candidate t n=if n>0 then let c=(generatetree t)in if not(findCycle c)||not(is_connexe c (getbase c)) then candidate t n 
+  else if (weight t)>(weight c) then candidate c (n-1) else candidate t (n-1) else t
